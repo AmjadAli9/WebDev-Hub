c50ca7908edfaa384dc817386ed0539ca994051a
Mohd Amjad Ali <2003amjadali@gmail.com>
2025-03-11 12:36:10 +0530

    Fix double router issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {  Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Code from "./pages/CodePad";
 import Tutorials from "./pages/Tutorial";
